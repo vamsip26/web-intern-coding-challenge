7ae304c7f2cc544ea0a25a0fb5b2de8c3d6f95a1
Brendan <admiralme@hotmail.com>
2017-04-17 17:16:23 -0700

    queries API and puts json object data into a list. Quick readme solution.

--- Pizzeria/src/js/pizza_finder.js
+++ Pizzeria/src/js/pizza_finder.js
@@ -1,1 +1,39 @@
 /* jQuery and JS for Pizza API */
+$(document).ready(function() {
+	$('#find').click(function() {
+		findPizza();
+		$(this).closest('form').find("input[type=text], textarea").val("");
+	});
+	$('#city').keyup(function(event) {
+		if(event.keyCode == 13){
+			findPizza();
+			$(this).closest('form').find("input[type=text], textarea").val("");
+		}
+	});
+	var findPizza = function() {
+		var location = $('#city').val();
+		console.log('location is: ', location);
+		if (!location.trim()) {
+			$('#validation').html("<h3 class='loading'>Please enter a city name</h3>");
+		} else {
+			$('#validation').html("<h3 class='loading'>Loading pizzeria's");
+			$.getJSON("http://shipt-pizza-api.herokuapp.com/api/v1/properties/search?city=" + location, function(json) {
+				if (json.length == 0) {
+					$('#validation').html("<h3 class='loading'>Nothing found for " + location + "</h3>");
+				} else {
+					console.log(json);
+					var total = "" + json.length;
+					var list = $('#pizza-list');
+					$('#validation').html("<h3 class='loading'>" + total + " pizzeria near " + location + "</h3>");
+					$.each(json, function(index, object) {
+						var restaurantName = json[index].properties.pizzeria;
+						var restaurantAddress = json[index].properties.address;
+						var item = "<li>" + restaurantName + " " + restaurantAddress + "</li>";
+						list.append(item);
+					});
+				}
+			});
+		}
+		return false;
+	}
+});
--- readme.md
+++ readme.md
@@ -7,11 +7,26 @@ Readme
 Tools
 -----------------
 * 'free' MAMP for a local server environment
+    - Download MAMP [here](https://www.mamp.info/en/downloads/) and place this project folder in the "htdocs" folder. Then start MAMP and open the WebStart page button. Click on "My Website" and navigate to the directory containing index.html.
+
 * 'free' Sublime 2 text editor
 
 
+Naming Conventions
+------------------
+File names have underscores
+class's and id's in CSS have hyphens
+variables and functions in JS have camel case
+
 Problem
 -------
+**Pizzeria**
+Create a user friendly web service for finding the latest and top rated pizza restaurants around the world!
+Design UX/UI that is simple enough to handle/understand for users of all ages.
+Requirements:
+	- Allow users to query either by city name or by restaurant name.
+	- List pizzeria's elegantly (possible Google Maps API showcase)
+	- Allow users to click on links in the list to gain additional information or start navigation to pizzeria
 
 Solution
 --------
